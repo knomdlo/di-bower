e63da9f3a80428f7680ffc06cc11ed713fa4b163
J <BanuPrakash.J@thomsonreuters.com>
2017-09-07 11:24:07 +0530

    Advancing the calci

--- dist/di-calci.js
+++ dist/di-calci.js
@@ -1,3 +1,23 @@
-function addNumbers(a,b) {
-	return a+b;
+document.getElementById("addButton").addEventListener("click", function() {
+  add();
+});
+
+document.getElementById("subButton").addEventListener("click", function() {
+  sub();
+});
+
+function add(x,y) {
+  var x = parseInt(document.getElementById('x').value);
+  var y = parseInt(document.getElementById('y').value);
+  var sum = x + y;
+
+  document.getElementById('result').innerHTML = sum;
+}
+
+function sub(x,y) {
+  var x = parseInt(document.getElementById('x').value);
+  var y = parseInt(document.getElementById('y').value);
+  var diff = x - y;
+
+  document.getElementById('result').innerHTML = diff;
 }
